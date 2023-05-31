53a54649a98e4853ed1726cfd50f8375b735fffc
heyyakash <akashsharma2002@gmail.com>
2023-05-31 18:12:43 +0530

    fixed login

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,14 @@ import { Inter } from 'next/font/google'
 import Canvas from '@/components/Canvas'
 import { useState } from 'react'
 import CanvasContainer from '@/components/CanvasContainer'
+import Login from '@/components/Login'
 
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
  <>
- <CanvasContainer />
+ <Login />
  </>
   )
 }
